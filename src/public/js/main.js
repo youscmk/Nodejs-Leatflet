66a8492bd04662cb5746a37e164fecf087c5449b
youscmk <thesteam2010@live.com>
2023-01-03 16:25:21 -0300

    c

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -13,6 +13,10 @@ const marker = L.marker([50.5, 30.5]); // kiev, ukraine
 marker.bindPopup('Hello There!');
 map.addLayer(marker);
 
+/*const marker2 = L.marker([70, 70.5]); // kiev, ukraine
+marker.bindPopup('Hello There!');
+map.addLayer(marker2);*/
+
 // Geolocation
 map.locate({enableHighAccuracy: true})
 map.on('locationfound', (e) => {
@@ -37,5 +41,29 @@ socket.on('newUserCoordinates', (coords) => {
   map.addLayer(newUserMarker);
 }); 
 
-map.addLayer(tile);
 
+
+map.locate({enableHighAccuracy: true})
+map.on('locationfound', (e) => {
+  const coords = [e.latlng.lat+100, e.latlng.lng+100.2];
+  const newMarker = L.marker(coords);
+  newMarker.bindPopup('You are Here!');
+  map.addLayer(newMarker);
+  socket.emit('userCoordinates', e.latlng);
+});
+
+// socket new User connected
+socket.on('newUserCoordinates', (coords) => {
+  console.log(coords);
+  const userIcon = L.icon({
+    iconUrl: '/img/icon2.png',
+    iconSize: [38, 42],
+  })
+  const newUserMarker = L.marker([coords.lat, coords.lng], {
+    icon: userIcon 
+  });
+  newUserMarker.bindPopup('New User!');
+  map.addLayer(newUserMarker);
+}); 
+
+map.addLayer(tile);
